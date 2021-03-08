52735f3fd900be355b49212876bf86c3c6c2cf50
wariboko <davis.waris@gmail.com>
2021-03-08 23:48:33 +0200

    "Updated"

--- wiki/settings.py
+++ wiki/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '192.168.100.12', 'localhost', '127.0.0.1', '192.168.100.17'
+    'localhost', '127.0.0.1', '192.168.100.17'
 ]
 
 
